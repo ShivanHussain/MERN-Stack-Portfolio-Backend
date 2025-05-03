6336aea653740eb208b8163b046ec3f7ac3b4cac
ShivanHussain <shivanhussain6@gmail.com>
2025-05-03 13:07:05 +0530

    Update app.js
    add localhost url

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use('*',
   cors({
-    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    origin: [
+              process.env.PORTFOLIO_URL,
+             process.env.DASHBOARD_URL,
+            "http://localhost:5173/",
+            "http://localhost:5173/"
+            ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
